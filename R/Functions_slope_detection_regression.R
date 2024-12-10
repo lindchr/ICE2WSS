927d046d06f24e7e1a510187a3e2201227571581
lindchr <131458705+lindchr@users.noreply.github.com>
2024-12-10 10:02:53 +0100

    Update Functions_slope_detection_regression.R

--- R/Functions_slope_detection_regression.R
+++ R/Functions_slope_detection_regression.R
@@ -1,3 +1,12 @@
+# This file is part of ICE2WSS
+# ICE2WSS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# [Full details at https://www.gnu.org/licenses/gpl-3.0.en.html]
+
+
+
 
 #' Find_slope
 #' Checks input data, call all other functions and saves output data to file
