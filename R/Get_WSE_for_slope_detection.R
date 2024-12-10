268ef11630fa71d66589bd8864d5e051846929ce
lindchr <131458705+lindchr@users.noreply.github.com>
2024-12-10 10:03:12 +0100

    Update Get_WSE_for_slope_detection.R

--- R/Get_WSE_for_slope_detection.R
+++ R/Get_WSE_for_slope_detection.R
@@ -1,3 +1,10 @@
+# This file is part of ICE2WSS
+# ICE2WSS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# [Full details at https://www.gnu.org/licenses/gpl-3.0.en.html]
+
 
 #' Calculate River water surface slopes using satellite data and river centre lines
 #'
