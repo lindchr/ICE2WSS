24462cc3584257de52e6866c8bad1ea83305f138
lindchr <131458705+lindchr@users.noreply.github.com>
2024-12-10 10:03:25 +0100

    Update Handle_SWOT_dat.R

--- R/Handle_SWOT_dat.R
+++ R/Handle_SWOT_dat.R
@@ -1,3 +1,11 @@
+# This file is part of ICE2WSS
+# ICE2WSS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# [Full details at https://www.gnu.org/licenses/gpl-3.0.en.html]
+
+
 ##############################################3
 #Extract data from SWORD
 ##############################################
